d48d8ecac8efd1b92c7da6cc85593cbfcefa8341
danialafiq03 <77504780+danialafiq03@users.noreply.github.com>
2021-05-22 02:35:27 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,13 +2,13 @@ from flask import Flask, render_template, render_template_string, request, sessi
 from flask_socketio import SocketIO, send
 import random
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'mysecret'
-socketio = SocketIO(app)
+# app.config['SECRET_KEY'] = 'mysecret'
+# socketio = SocketIO(app)
 
-@app.before_request
-def before_request():
-    random_num = random.randint(0, 10000)
-    session['guest_id'] = random_num
+# @app.before_request
+# def before_request():
+#     random_num = random.randint(0, 10000)
+#     session['guest_id'] = random_num
 
 @app.route('/')
 def home():
@@ -37,12 +37,12 @@ def index():
 def chat():
     return render_template('chat.html')
 
-@socketio.on('message')
-def handleMessage(msg):
-    if msg == "has connected!":
-        send('Guest #' + str(session['guest_id']) + ' ' + msg, broadcast=True)
-    else:
-        send('Guest #' + str(session['guest_id']) + ': ' + msg, broadcast=True)
+# @socketio.on('message')
+# def handleMessage(msg):
+#     if msg == "has connected!":
+#         send('Guest #' + str(session['guest_id']) + ' ' + msg, broadcast=True)
+#     else:
+#         send('Guest #' + str(session['guest_id']) + ': ' + msg, broadcast=True)
 
 if __name__ == '__main__':
-    socketio.run(app)
+    app.run()
