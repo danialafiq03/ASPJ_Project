b4c01f5df22a2c8139c0e97f5cb7efe024ae72e3
danialafiq03 <77504780+danialafiq03@users.noreply.github.com>
2021-05-22 03:20:39 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -45,4 +45,4 @@ def handleMessage(msg):
         send('Guest #' + str(session['guest_id']) + ': ' + msg, broadcast=True)
 
 if __name__ == '__main__':
-    socketio.run(app)
+    socketio.run(app, port='46919')
